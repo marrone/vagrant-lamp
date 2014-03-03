59ae862574f1e3580fcd49fa867349235b38c21a
Mike Marrone <=>
2014-03-03 13:42:16 -0500

    enabled remote debugging in xdebug php

--- modules/php/manifests/pecl.pp
+++ modules/php/manifests/pecl.pp
@@ -4,4 +4,9 @@ class php::pecl {
   # TBD: will be added later...
 
   # install xdebug
+  file { "/etc/php5/conf.d/xdebug.ini":
+    ensure => present,
+    source => "/vagrant/manifests/xdebug.ini",
+    require => Package["php5-xdebug"],
+  }
 }
